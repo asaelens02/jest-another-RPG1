caac6c9bdbf6066f00138003d5730aaddefa96fc
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-08 23:27:20 -0500

    update player health

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -19,3 +19,43 @@ test ('creates a player object', () => {
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
+test ("gets player's stats as an object", () =>{
+
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+
+});
+
+test ('gets inventory from player or returns false',() =>{
+
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
+test ('gets player health value',() => {
+
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test ('subtracts from players health',() =>{
+    const player =new Player('Dave');
+    const oldHealth= player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth-5);
+    player.reduceHealth(99999);
+    expect(player.health).toBe (0);
+});
